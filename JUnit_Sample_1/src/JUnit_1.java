7e32d16f129cf27ba89924e7cc1fa029f2f03aed
pranayXoX <pranay.dmnsns@gmail.com>
2022-11-17 21:55:43 +0530

    Simple Test Case

--- JUnit_Sample_1/src/JUnit_1.java
+++ JUnit_Sample_1/src/JUnit_1.java
@@ -1,12 +1,25 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.*;
+
 public class JUnit_1 {
+	String username;
+	String password;
 	@Test
-	public void TestClass() {
-		
+	static void TestClass(String username, String password) {
+		System.out.print("\nTEST CASE\n");
+		assertEquals(password,"Pranay101");
+		assertEquals(username,"Pranay_Ranjan");
 	}
 	static public void main(String[] args) {
-		
+		Scanner s=new Scanner(System.in);
+		JUnit_1 obj=new JUnit_1();
+		System.out.print("\nEnter Username:");
+		obj.username=s.nextLine();
+		System.out.print("\nEnter Password:");
+		obj.password=s.nextLine();
+		TestClass(obj.username,obj.password);
 	}
-
 }
