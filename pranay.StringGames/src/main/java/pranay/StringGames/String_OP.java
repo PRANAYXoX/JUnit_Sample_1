ec6b3af2c2a51ccaa80a0523dd4fa4a78b540fc6
pranayXoX <pranay.dmnsns@gmail.com>
2022-11-22 08:48:51 +0530

    String to array Test Case implemented!

---
+++ pranay.StringGames/src/main/java/pranay/StringGames/String_OP.java
@@ -0,0 +1,15 @@
+package pranay.StringGames;
+
+public class String_OP {
+	
+	public String[] abbrebiator(String name) {
+		String[] abb=name.split(" ");	
+		return abb;
+	}
+	
+	/*static public void main(String[] args) {
+		String name="Robin Singh";
+		abbrebiator(name);
+	}*/
+
+}
